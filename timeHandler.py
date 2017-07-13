e1fc1be7ad3902a9b3ed28c1d37f30aca722d07b
Matthias <mholetzko@gmx.net>
2017-07-13 02:09:43 +0100

    initial commit

---
+++ timeHandler.py
@@ -0,0 +1,90 @@
+# -*- coding: utf-8 -*-
+
+import datetime as t
+
+class wordqlockTimeHandler():
+    
+    def __init__(self):
+        self.currentTime = t.datetime.now();
+        self.activeIndices = [];
+
+        
+    def updateTime(self):
+        self.currentTime = t.datetime.now();
+
+    def getTableTime(self,hour):
+        if self.currentTime.minute >= 30:
+            if hour < 24:
+                lochour = hour + 1
+            else:
+                lochour = 0
+            return lochour%12;
+        else:
+            return hour%12;
+                
+
+    def getCurrentMinuteCfg(self):
+        self.updateTime()
+        uIdx = 0;
+        for i in range(len(self.minutecfgs) - 1 ):
+            if self.currentTime.minute >= self.minutecfgs[i][1] and self.currentTime.minute < self.minutecfgs[i+1][1]:
+                   
+                #print("INDEX" + str(i) + self.minutecfgs[i][0]);
+                uIdx = i;
+
+        return self.minutecfgs[uIdx][2]
+
+    def getCurrentHourCfg(self):
+        self.updateTime()
+        table = self.hourcfgs[self.getTableTime(self.currentTime.hour)][2]
+        #todo handle special case EIN(S)
+        if self.currentTime.minute < 5 and self.currentTime.hour == 1:
+            table = table[:-1];
+        return table
+        
+    def getCurrentMinuteOffset(self):
+        self.updateTime()
+        offset = 0;
+        for i in range(len(self.minutecfgs) - 1 ):
+            if self.currentTime.minute >= self.minutecfgs[i][1] and self.currentTime.minute < self.minutecfgs[i+1][1]:
+                   
+                #print("INDEX" + str(i) + self.minutecfgs[i][0]);
+                offset = self.currentTime.minute - self.minutecfgs[i][1];
+        print(offset)
+        return [x for x in range(offset)]
+
+    def getCurrentSecondOffset(self):
+        self.updateTime()
+        return self.currentTime.second
+
+    def setActiveByIndex(self,row,col):
+        
+        bRet = False;
+        for (i,j) in self.staticIndices:
+            if i == row and j == col:
+                #print(self.currentTime)
+                bRet = True;
+        #todo check for special cases
+        for (m,n) in self.getCurrentMinuteCfg():
+             if m == row and n == col:
+                #print(self.currentTime)
+                bRet = True;
+                
+        for (p,q) in self.getCurrentHourCfg():
+             if p == row and q == col:
+                #print(self.currentTime)
+                bRet = True;
+        
+        if self.currentTime.minute < 5:
+            #print UHR
+            if row == 9 and col in [8,9,10]:
+                bRet = True;
+        return bRet;
+
+        
+        
+        
+        
+     
+        
+        
---
+++ visu.py
@@ -0,0 +1,92 @@
+#!/usr/bin/python3
+# -*- coding: utf-8 -*-
+
+"""
+ZetCode PyQt5 tutorial 
+
+In this example, we draw text in Russian azbuka.
+
+author: Jan Bodnar
+website: zetcode.com 
+last edited: September 2015
+"""
+
+import sys
+from PyQt5.QtWidgets import QApplication,QMainWindow
+from PyQt5.QtGui import QPainter, QColor, QFont
+from PyQt5.QtCore import Qt, QTimer
+
+from layout      import wordqlockLayout
+
+class WordqlockVisu(QMainWindow):
+    
+    def __init__(self):
+        super().__init__()
+        
+        self.timer = QTimer(); 
+        self.timer.setInterval(100)
+        self.initUI()
+        
+        
+    def initUI(self):      
+
+        self.layout = wordqlockLayout();
+        self.setStyleSheet("background-color: black")
+        self.setGeometry(100, 100, 220, 250)
+        self.setWindowTitle('WordQlockLayout')  
+        self.timer.timeout.connect(self.update)
+        self.timer.start();
+
+    def paintEvent(self, event):
+        print("PAINT EVENT")
+        qp = QPainter()
+        qp.begin(self)
+        self.drawText(event, qp)
+        self.drawRectangle(qp)
+        qp.end()
+        
+    def drawRectangle(self, qp):
+      
+        col = QColor(0, 0, 0)
+        col.setNamedColor('#d4d4d4')
+        qp.setPen(col)
+        qp.setOpacity(0.25)
+        qp.setBrush(Qt.darkGray)
+        qp.drawRect(10, 10, 200, 230)
+        for x in self.layout.getCurrentMinuteOffset():
+            qp.setBrush(Qt.red)
+            qp.drawRect(15 + x*60, 15, 10, 10)
+        qp.setBrush(Qt.red)
+        qp.drawRect(15, 220, 10+ 3*self.layout.getCurrentSecondOffset(), 10)
+            
+
+        
+    def drawText(self, event, qp):
+        offset = 3;
+        offsety = 3;
+        for i in range(len(self.layout.letterField)):
+            offsety +=10;
+            offset = 3;
+            for j in range(len(self.layout.letterField[i])):
+                offset += 10; 
+                if(self.layout.setActiveByIndex(i,j)):
+                    qp.setPen(Qt.white)
+                else:
+                    qp.setPen(Qt.darkGray)
+                qp.setFont(QFont('Decorative', 10))
+                qp.drawText(5+offset ,10+offsety, 20+offset, 30+offsety, Qt.AlignCenter, self.layout.letterField[i][j])        
+        #print(self.layout.letterField);
+        #print(len(self.layout.letterField)); 
+
+    def update(self):
+        print(" ############################### Here")
+        print(" ******************************* Here")
+        self.repaint()
+
+        
+if __name__ == '__main__':
+    
+    app = QApplication(sys.argv)
+    ex = WordqlockVisu()
+    ex.show()
+    sys.exit(app.exec_())
