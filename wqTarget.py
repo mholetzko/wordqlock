58e7a0b7fe23c22687b0b23b9e4cf604cbe9b4ea
Matthias Holetzko <mholetzko@gmx.net>
2020-11-23 21:59:00 +0100

    brighterrrr

--- wqTarget.py
+++ wqTarget.py
@@ -20,7 +20,7 @@ class wqTargetApp(SampleBase):
         self.debug = True
         self.period = updatePeriod
         self.init = False
-        self.intensity = 150
+        self.intensity = 200
         self.process()
         self.offset_canvas = self.matrix.CreateFrameCanvas()
 
