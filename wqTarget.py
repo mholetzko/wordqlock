f973e4e2870dcf78d695942aa65c4df9099d1a60
Matthias Holetzko <mholetzko@gmx.net>
2020-11-23 20:56:02 +0100

    reduced brightness

--- wqTarget.py
+++ wqTarget.py
@@ -20,6 +20,7 @@ class wqTargetApp(SampleBase):
         self.debug = True
         self.period = updatePeriod
         self.init = False
+        self.intensity = 100
         self.process()
         self.offset_canvas = self.matrix.CreateFrameCanvas()
 
@@ -40,10 +41,10 @@ class wqTargetApp(SampleBase):
                 pixlIdx = 0
                 for pixel in row:
                     if pixel != 0:
-                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx,      2*rowIdx+rowIdx,255,255,255)
-                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx+1,    2*rowIdx+rowIdx,255,255,255)
-                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx,      2*rowIdx+rowIdx+1,255,255,255)
-                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx+1,    2*rowIdx+rowIdx+1,255,255,255)
+                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx,      2*rowIdx+rowIdx,self.intensity,self.intensity,self.intensity)
+                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx+1,    2*rowIdx+rowIdx,self.intensity,self.intensity,self.intensity)
+                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx,      2*rowIdx+rowIdx+1,self.intensity,self.intensity,self.intensity)
+                        self.offset_canvas.SetPixel(2*pixlIdx+pixlIdx+1,    2*rowIdx+rowIdx+1,self.intensity,self.intensity,self.intensity)
                     pixlIdx = pixlIdx + 1
                 rowIdx = rowIdx + 1
             self.offset_canvas = self.matrix.SwapOnVSync(self.offset_canvas)
